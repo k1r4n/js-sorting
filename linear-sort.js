14cae2d5435c54c03d5b3fea5cec312148dfe9ac
k1r4n <kiranrajaram@outlook.com>
2020-07-30 14:48:58 +0530

    linear-sort added

---
+++ linear-sort.js
@@ -0,0 +1,17 @@
+const linearSort = (arr) => {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                const temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(linearSort([4, 2, 1, 3, 7, 6, 5, 3, 2, 10, 9]));
